61d08a9baa005e0121fbc13cee9bb55009105fd4
Andrii Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-09-20 17:42:48 +0300

    '1111

--- client/test.js
+++ client/test.js
@@ -1,41 +1,39 @@
-// import { exec } from 'child_process';
-const exec = require('child_process')
+// const exec = require('child_process')
+
+// const commitName = process.argv[2];
+
+// const cmd = `npm run build && cd .. && git add . && git commit -m '${commitName}' && git push`;
+
+// exec.exec(cmd, (error, stdout, stderr) => {
+//     if (error) {
+//         console.error(`exec error: ${error}`);
+//         return;
+//     }
+
+//     console.log('success')
+// });
+
+const { promisify } = require('util');
+const { exec } = require('child_process');
+const execAsync = promisify(exec);
 
 const commitName = process.argv[2];
 
-const cmd = `npm run build && cd .. && git add . && git commit -m '${commitName}' && git push`;
-//
-exec.exec(cmd, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-
-    console.log('success')
-});
-
 const start = async () => {
-    await exec.exec('npm run build ', (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          console.error(`build error`);
-          return;
-        }
+    // await execAsync('npm run build ', (error, stdout, stderr) => {
+    //     if (error) {
+    //         console.error(`exec error: ${error}`);
+    //         console.error(`build error`);
+    //         return;
+    //     }
+        
+    //     console.log('buid success')
+    // });
+    console.log('1111')
       
-        console.log('buid success')
-      });
-      
-    await  exec.exec('cd .. && git add .', (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          console.error(`git add . fail`);
-          return;
-        }
-      
-        console.log('git add . successfully')
-      });
-      
-    await  exec.exec(`git commit -m '${commitName}`, (error, stdout, stderr) => {
+    await  execAsync('cd .. && git add .');
+      console.log('2222')
+    await  execAsync(`git commit -m '${commitName}`, (error, stdout, stderr) => {
         if (error) {
           console.error(`exec error: ${error}`);
           console.error(`git commit fail`);
@@ -45,17 +43,27 @@ const start = async () => {
         console.log('git commit successfully')
       });
       
-    await  exec.exec(`git push`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          console.error(`git push fail`);
-          return;
-        }
+    // await  execAsync(`git push`, (error, stdout, stderr) => {
+    //     if (error) {
+    //       console.error(`exec error: ${error}`);
+    //       console.error(`git push fail`);
+    //       return;
+    //     }
       
-        console.log('git push successfully')
-      });
+    //     console.log('git push successfully')
+    //   });
       
       console.log('success')
 }
 
 // start()
+start().then(() => {
+    console.log('All commands completed');
+  }).catch((error) => {
+    console.error(`Error: ${error}`);
+  });
+  
+  
+  
+  
+  
