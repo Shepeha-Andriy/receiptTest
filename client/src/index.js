1e9ccd5c9c5a84d97a40415fcc992046dcf5880f
Andrii Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-09-20 16:35:23 +0300

    test

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,10 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store'
 
 const BuildTest = 1111
+const BuildTest3 = 1111
 const BuildTest2 = BuildTest
 console.log(BuildTest2)
+console.log(BuildTest3)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
