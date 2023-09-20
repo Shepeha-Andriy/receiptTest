c1683ff81480a8a2b87b860578688ebd3c66417f
Andrii Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-09-20 17:56:47 +0300

    test2214324

--- client/test.js
+++ client/test.js
@@ -1,59 +1,59 @@
-// const exec = require('child_process')
-
-// const commitName = process.argv[2];
-
-// const cmd = `npm run build && cd .. && git add . && git commit -m '${commitName}' && git push`;
-
-// exec.exec(cmd, (error, stdout, stderr) => {
-//     if (error) {
-//         console.error(`exec error: ${error}`);
-//         return;
-//     }
-
-//     console.log('success')
-// });
-
-const { promisify } = require('util');
-const { exec } = require('child_process');
-const execAsync = promisify(exec);
+const exec = require('child_process')
 
 const commitName = process.argv[2];
 
-const start = async () => {
-    const build = await execAsync('npm run build ');
-    if (typeof build === 'string') {
-        console.log(build)
-    } else {
-        console.log('build success')
-    }
-      
-    const gitAddRes = await  execAsync('cd .. && git add .');
-    if (typeof gitAddRes === 'string') {
-        console.log(gitAddRes)
-    } else {
-        console.log('git add . success')
+const cmd = `git config --global user.email "n.vaskiv@medias.com.ua" && git config --global user.name "Build Bot"`;
+
+exec.exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
     }
 
-    const gitCommitRes = await  execAsync(`git commit -m '${commitName}`);
-    if (typeof gitCommitRes === 'string') {
-        console.log(gitCommitRes)
-    } else {
-        console.log('git commit success')
-    }
-      
-    const gitPushRes = await  execAsync(`git push`);
-    if (typeof gitPushRes === 'string') {
-        console.log(gitPushRes)
-    } else {
-        console.log('git push success')
-    }
-}
+    console.log('success')
+});
 
-start().then(() => {
-    console.log('All commands completed');
-  }).catch((error) => {
-    console.error(`Error: ${error}`);
-  });
+// const { promisify } = require('util');
+// const { exec } = require('child_process');
+// const execAsync = promisify(exec);
+
+// const commitName = process.argv[2];
+
+// const start = async () => {
+//     const build = await execAsync('npm run build ');
+//     if (typeof build === 'string') {
+//         console.log(build)
+//     } else {
+//         console.log('build success')
+//     }
+      
+//     const gitAddRes = await  execAsync('cd .. && git add .');
+//     if (typeof gitAddRes === 'string') {
+//         console.log(gitAddRes)
+//     } else {
+//         console.log('git add . success')
+//     }
+
+//     const gitCommitRes = await  execAsync(`git commit -m '${commitName}`);
+//     if (typeof gitCommitRes === 'string') {
+//         console.log(gitCommitRes)
+//     } else {
+//         console.log('git commit success')
+//     }
+      
+//     const gitPushRes = await  execAsync(`git push`);
+//     if (typeof gitPushRes === 'string') {
+//         console.log(gitPushRes)
+//     } else {
+//         console.log('git push success')
+//     }
+// }
+
+// start().then(() => {
+//     console.log('All commands completed');
+//   }).catch((error) => {
+//     console.error(`Error: ${error}`);
+//   });
   
   
   
