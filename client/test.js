14f79d15f04d5711cf59b0e4c9593f7815a77eac
Andrii Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-09-20 17:25:13 +0300

    'test213421321

--- client/test.js
+++ client/test.js
@@ -1,15 +1,48 @@
 // import { exec } from 'child_process';
 const exec = require('child_process')
 
-const serverName = process.argv[2];
+const commitName = process.argv[2];
 
-const cmd = `cd .. && git add . && git commit -m '${serverName}' && git push`;
+const cmd = `npm run build && cd .. && git add . && git commit -m '${commitName}' && git push`;
 
-exec.exec(cmd, (error, stdout, stderr) => {
+exec.exec('npm run build ', (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
+    console.error(`build error`);
     return;
   }
 
-  console.log('end successfully')
+  console.log('buid success')
 });
+
+exec.exec('cd .. && git add .', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    console.error(`git add . fail`);
+    return;
+  }
+
+  console.log('git add . successfully')
+});
+
+exec.exec(`git commit -m '${commitName}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    console.error(`git commit fail`);
+    return;
+  }
+
+  console.log('git commit successfully')
+});
+
+exec.exec(`git push`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    console.error(`git push fail`);
+    return;
+  }
+
+  console.log('git push successfully')
+});
+
+console.log('success')
