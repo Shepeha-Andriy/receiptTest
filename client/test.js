1b84fcd4c8a407494266d554f1280881c28b6430
Andrii Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-09-20 17:27:51 +0300

    'test213421321'

--- client/test.js
+++ client/test.js
@@ -5,44 +5,57 @@ const commitName = process.argv[2];
 
 const cmd = `npm run build && cd .. && git add . && git commit -m '${commitName}' && git push`;
 
-exec.exec('npm run build ', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    console.error(`build error`);
-    return;
-  }
+exec.exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
 
-  console.log('buid success')
+    console.log('success')
 });
 
-exec.exec('cd .. && git add .', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    console.error(`git add . fail`);
-    return;
-  }
+const start = async () => {
+    await exec.exec('npm run build ', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          console.error(`build error`);
+          return;
+        }
+      
+        console.log('buid success')
+      });
+      
+    await  exec.exec('cd .. && git add .', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          console.error(`git add . fail`);
+          return;
+        }
+      
+        console.log('git add . successfully')
+      });
+      
+    await  exec.exec(`git commit -m '${commitName}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          console.error(`git commit fail`);
+          return;
+        }
+      
+        console.log('git commit successfully')
+      });
+      
+    await  exec.exec(`git push`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          console.error(`git push fail`);
+          return;
+        }
+      
+        console.log('git push successfully')
+      });
+      
+      console.log('success')
+}
 
-  console.log('git add . successfully')
-});
-
-exec.exec(`git commit -m '${commitName}`, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    console.error(`git commit fail`);
-    return;
-  }
-
-  console.log('git commit successfully')
-});
-
-exec.exec(`git push`, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    console.error(`git push fail`);
-    return;
-  }
-
-  console.log('git push successfully')
-});
-
-console.log('success')
+// start()
